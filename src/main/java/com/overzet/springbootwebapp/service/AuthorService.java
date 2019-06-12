6ed785a0e407c98203275f3a7d9e6349f3f6528c
Overzet <overzet@gmail.com>
2019-06-12 16:15:16 +0200

    Update not working

---
+++ src/main/java/com/overzet/springbootwebapp/service/AuthorService.java
@@ -0,0 +1,4 @@
+package com.overzet.springbootwebapp.service;
+
+public interface AuthorService {
+}
--- src/main/java/com/overzet/springbootwebapp/service/AuthorServiceImpl.java
+++ src/main/java/com/overzet/springbootwebapp/service/AuthorServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class AuthorServiceImpl {
+public class AuthorServiceImpl implements AuthorService {
 
 
     @Autowired
