5906b59d27065bb42100366ad336f470ec8efe6f
doProgramming <petrovicstrahinja@yahoo.com>
2019-06-18 10:33:17 +0200

    Clean code changes

--- src/main/java/com/overzet/SpringbootwebappApplication.java
+++ src/main/java/com/overzet/SpringbootwebappApplication.java
@@ -1,4 +1,4 @@
-package com.overzet.springbootwebapp.author;
+package com.overzet.springbootwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
